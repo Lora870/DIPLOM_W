ee15354911d02617b61a6cc22b340e5768540f8c
Lora870 <rionataly@yandex.ru>
2025-12-12 05:41:41 +0500

    Тест "Читай Город"

--- README.md
+++
@@ -1,1 +0,0 @@
-# DIPLOM_W
---
+++ Test/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium import webdriver
+
+#Фикстура для инициализации и закрытия драйвера.
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.implicitly_wait(50)
+    yield driver
+    driver.quit()
